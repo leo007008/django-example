d12b7402b56a187a808266416f08999433dc93d4
leo <lichaowudi007@163.com>
2016-09-02 17:27:22 +0800

    custom_ the admin site

--- 1-creat-a-blog/myblog/blog/admin.py
+++ 1-creat-a-blog/myblog/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Post
 
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','slug','auther','publish','status')
+    list_filter = ('status','created','auther','publish')
+    search_fields = ('title','body')
+    prepopulated_fields = {'slug':('title',)}
+    raw_id_fields = ('auther',)
+    date_hierarchy = 'publish'
+    ordering = ['status','publish']
+admin.site.register(Post,PostAdmin)
 # Register your models here.
